961da5011943648c62bc500a588ee7dc235ac9dd
Jeremy Thurgood <jeremy@praekelt.com>
2016-02-08 12:43:36 +0200

    Some better tests for gluster::repo.

--- spec/classes/repo_spec.rb
+++ spec/classes/repo_spec.rb
@@ -7,7 +7,24 @@ describe 'gluster::repo' do
         facts
       end
 
-      it { should compile }
+      describe 'default to' do
+        it { is_expected.to contain_apt__ppa('ppa:gluster/glusterfs-3.7') }
+      end
+
+      describe 'when unmanaged' do
+        let(:params) { {:manage => false} }
+        it { should_not contain_apt__ppa('ppa:gluster/glusterfs-3.7') }
+      end
+
+      describe 'when set to an invalid source' do
+        let(:params) { {:source => 'foo.rb'} }
+        it { should compile.and_raise_error(/'foo.rb' .* not supported/) }
+      end
+
+      describe 'when run on an unsupported OS' do
+        let(:facts) { {:osfamily => 'iOS'} }
+        it { should compile.and_raise_error(/No repository .* 'iOS'/) }
+      end
     end
   end
 end
